d200c656870eb7798ffcf01dbc35f4614740653d
Daniel Eichhorn (Energy4U) <Eichhorn.Daniel287@gmail.com>
2025-03-25 21:23:40 +0100

    Update Navbar and page components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import Services from './components/sections/Services';
 import Stats from './components/sections/Stats';
 import Testimonials from './components/sections/Testimonials';
 import WhatWeOffer from './components/sections/WhatWeOffer';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
       <Hero />
       <Mission />
       <Services />
