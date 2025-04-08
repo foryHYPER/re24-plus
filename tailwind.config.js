25951f3ca046bb822c0ce02ea33119953361469c
foryHyper <Eichhorn.Daniel287@gmail.com>
2025-04-08 17:32:59 +0200

    Remove  Daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
       './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,5 +8,5 @@ module.exports = {
     theme: {
       extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [],
   }
